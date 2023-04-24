ecf942346e83544ecbc34c9c03bcc204401e5cf4
Anh <anhlt4@tinhvan.com>
2023-04-24 10:46:39 +0700

    Add DbContext and product service

---
+++ RabbitMqProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RabbitMqProductAPI.Models;
+
+namespace RabbitMqProductAPI.Data
+{
+	public class AppDbContext : DbContext
+	{
+		protected readonly IConfiguration _configuration;
+		public AppDbContext(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+		}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
